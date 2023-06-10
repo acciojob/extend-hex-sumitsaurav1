07c0a15b0e094c4cc1cef9ea7690873c39d70d93
Sumit saurav <117914094+sumitsaurav1@users.noreply.github.com>
2023-06-10 12:45:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const extendHex = (shortHex) => {
-  // write your code here
-};
+    // write your code here
+    let result = "#";
+    for(let i=1;i<shortHex.length;i++){
+        result +=shortHex[i];
+        result +=shortHex[i];
+    }
+    return result;
 
-// Do not change the code below.
+  };
+  
+  // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
